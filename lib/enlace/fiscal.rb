c2cd09a7d293bab37670664efee984319043714d
Mario A Chavez <mario.chavez@gmail.com>
2015-02-12 13:19:17 -0600

    Repository initialization

---
+++ lib/enlace/fiscal.rb
@@ -0,0 +1,7 @@
+require "enlace/fiscal/version"
+
+module Enlace
+  module Fiscal
+    # Your code goes here...
+  end
+end
---
+++ lib/enlace/fiscal/version.rb
@@ -0,0 +1,5 @@
+module Enlace
+  module Fiscal
+    VERSION = "0.0.1"
+  end
+end
